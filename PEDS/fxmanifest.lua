1e109219cf076ff44fb6bc781375ce33afd0cd96
floppystudiodev <floppystudiodev@gmail.com>
2024-07-25 23:52:48 +0000

    peds

---
+++ PEDS/fxmanifest.lua
@@ -0,0 +1,17 @@
+fx_version 'cerulean'
+game 'gta5'
+
+files {
+    'stream/*.ytd',
+    'stream/*.ytf',
+    'stream/*.ydd',
+    'stream/*.ymt',
+    'stream/*.yft',
+    'data/peds.meta',
+    'data/pedalternatevariations.meta',
+    'data/pedpersonality.meta'
+}
+
+data_file 'PED_METADATA_FILE' 'data/peds.meta'
+data_file 'PED_VARIATION_FILE' 'data/pedalternatevariations.meta'
+data_file 'PED_PERSONALITY_FILE' 'data/pedpersonality.meta'

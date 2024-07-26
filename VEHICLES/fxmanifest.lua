ab9e58b60e5dbc120d59d270e5f83ad2cfa2bbb6
floppystudiodev <floppystudiodev@gmail.com>
2024-07-26 00:05:02 +0000

    vehicles

---
+++ VEHICLES/fxmanifest.lua
@@ -0,0 +1,20 @@
+fx_version 'cerulean'
+game 'gta5'
+
+files {
+    'data/vehicles.meta',
+    'data/carvariations.meta',
+    'data/carcols.meta',
+    'data/handling.meta',
+    'data/dlctext.meta',
+    'data/loadouts.meta',
+    'data/vehiclelayouts.meta'
+}
+
+data_file 'VEHICLE_METADATA_FILE' 'data/vehicles.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'data/carvariations.meta'
+data_file 'CARCOLS_FILE' 'data/carcols.meta'
+data_file 'HANDLING_FILE' 'data/handling.meta'
+data_file 'DLC_TEXT_FILE' 'data/dlctext.meta'
+data_file 'LOADOUTS_FILE' 'data/loadouts.meta'
+data_file 'VEHICLE_LAYOUTS_FILE' 'data/vehiclelayouts.meta'

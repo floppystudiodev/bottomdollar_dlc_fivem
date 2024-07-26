d616b326b9fa7e232d48b8a15a3c46d164844f34
floppystudiodev <floppystudiodev@gmail.com>
2024-07-26 00:06:12 +0000

    weapons

---
+++ WEAPONS/fxmanifest.lua
@@ -0,0 +1,20 @@
+fx_version 'cerulean'
+game 'gta5'
+
+files {
+    'stream/*.ytd',
+    'stream/*.ydr',
+    'data/loadouts.meta',
+    'data/weapon_stunrod.meta',
+    'data/weaponanimations.meta',
+    'data/weaponarchetypes.meta',
+    'data/contentunlocks.meta',
+    'content.xml',
+    'setup2.xml'
+}
+
+data_file 'WEAPON_METADATA_FILE' 'data/weapon_stunrod.meta'
+data_file 'WEAPON_ANIMATIONS_FILE' 'data/weaponanimations.meta'
+data_file 'WEAPON_ARCHETYPES_FILE' 'data/weaponarchetypes.meta'
+data_file 'LOADOUTS_FILE' 'data/loadouts.meta'
+data_file 'CONTENT_UNLOCKING_META_FILE' 'data/contentunlocks.meta'
